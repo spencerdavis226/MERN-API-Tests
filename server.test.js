842d649a9d63ae33e49247cddf219c1eec91896c
Spencer Davis <spencerdavis226@gmail.com>
2025-03-11 09:32:50 -0400

    Added server.test.js with simple GET test

---
+++ server.test.js
@@ -0,0 +1,12 @@
+const request = require('supertest'); // Simulates HTTP requests
+const chai = require('chai'); // Used for assertions (checking response status and JSON return matches)
+const expect = chai.expect;
+const app = require('./server');
+
+describe('GET /', () => {
+  it('should return a JSON message confirming the API is working', async () => {
+    const res = await request(app).get('/');
+    expect(res.status).to.equal(200);
+    expect(res.body).to.deep.equal({ message: 'API is working' });
+  });
+});
